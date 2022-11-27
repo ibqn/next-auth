e5c23bcc52200bae26d28b0c818025adb90b77f6
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-11-27 15:38:02 +0100

    clean up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{js,jsx}'],
+  content: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
