a1fb647217c69baf584a5fccac8cc86f4a23a910
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-12-04 17:12:51 +0100

    add component for social signin

---
+++ src/components/social-signin.jsx
@@ -0,0 +1,27 @@
+import { FaGoogle, FaFacebook } from 'react-icons/fa'
+import { Auth } from 'aws-amplify'
+
+export const SocialSignIn = () => {
+  return (
+    <div className="flex flex-col">
+      <button
+        className="mt-10 focus:outline-none"
+        onClick={() => Auth.federatedSignIn({ provider: 'Google' })}
+      >
+        <div className="flex items-center justify-center rounded-full border border-gray-300 p-2 hover:border-pink-700">
+          <FaGoogle size="38" className="h-8 text-red-600" />
+          <p className="ml-3">Sign in with Google</p>
+        </div>
+      </button>
+      <button
+        className="mt-4 focus:outline-none"
+        onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}
+      >
+        <div className="flex items-center justify-center rounded-full border border-gray-300 p-2 hover:border-pink-700">
+          <FaFacebook size="38" className="h-8 text-blue-600" />
+          <p className="ml-3">Sign in with Facebook</p>
+        </div>
+      </button>
+    </div>
+  )
+}
