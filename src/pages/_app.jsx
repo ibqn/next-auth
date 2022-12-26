0cdf211be814646de42e0b9fafcdf1227db2b1fc
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-12-26 17:00:54 +0100

    configure amplify

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,14 @@ import { Header } from 'components/header'
 import { Wrapper } from 'components/wrapper'
 import 'styles/globals.css'
 
+import Amplify from 'aws-amplify'
+import config from 'aws-exports'
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+})
+
 function MyApp({ Component, pageProps }) {
   return (
     <Wrapper>
