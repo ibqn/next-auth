ddb6d089a618604569b7acb07e67da9af8774ee7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2022-12-29 18:46:51 +0100

    improve header links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,35 @@
 import Link from 'next/link'
 
+const navigationLinks = [
+  {
+    name: 'home',
+    href: '/',
+  },
+  {
+    name: 'profile',
+    href: '/profile',
+  },
+  {
+    name: 'protected',
+    href: '/protected',
+  },
+]
+
 export const Header = () => {
   return (
-    <nav className="flex border-b border-gray-300 py-4 px-4">
-      <Link
-        href="/"
-        className="py-2 text-base font-medium leading-6 transition-colors duration-200 hover:text-gray-600"
-      >
-        Home
-      </Link>
-      <Link
-        href="/profile"
-        className="ml-10 py-2 text-base font-medium leading-6 transition-colors duration-200 hover:text-gray-600"
-      >
-        Profile
-      </Link>
-      <Link
-        href="/protected"
-        className="ml-10 py-2 text-base font-medium leading-6 transition-colors duration-200 hover:text-gray-600"
-      >
-        Protected
-      </Link>
+    <nav className="flex gap-10 border-b border-gray-300 py-4 px-4">
+      {navigationLinks?.map((item, index) => {
+        const { name, href } = item
+        return (
+          <Link
+            key={index}
+            href={href}
+            className="py-2 text-base font-medium capitalize leading-6 transition-colors duration-200 hover:text-gray-600"
+          >
+            {name}
+          </Link>
+        )
+      })}
     </nav>
   )
 }
